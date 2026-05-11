f6a1b46c744b07d7b49a4db44507c85b6083e86a
Arjun Rathi <rathiarjun053@gmail.com>
2026-05-12 00:00:41 +0530

    Time: 5 ms (79.29%), Space: 45.9 MB (53.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false; // Negative numbers are not palindromes
+        }
+        
+        int original = x;
+        int reversed = 0;
+        
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121)); // true
+        System.out.println(solution.isPalindrome(-121)); // false
+        System.out.println(solution.isPalindrome(10)); // false
+    }
+}
